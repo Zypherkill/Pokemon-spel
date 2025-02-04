6539daf54b85bd6a393367c5f94b04ac606f9c67
Robin-Torp <3kdyhfe24.torpro@folkuniversitetet.nu>
2025-02-04 14:42:13 +0100

    Fixa så pokemon kan röra sig samt bli fångade

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,53 @@
 "use strict";
 const log = (msg) => console.log(msg);
 
+const oGameData = {
+    pokemonNumbers : [],
+    nmbrOfCaughtPokemons : 0,
+    startTime : 0,
+    endTime : 0,
+    timerId : null,
+    trainerName : '',
+    trainerAge : 0,
+    trainerGender : '',
+    // Metod som nollställer datan i oGameData
+    init : () => {
+        pokemonNumbers = [];
+        nmbrOfCaughtPokemons = 0;
+        startTime = 0;
+        endTime = 0;
+        timerId = null;
+        nmbrOfSeconds = 0;
+        trainerName = '';
+        trainerAge = 0;
+        trainerGender = '';
+    },
+    // Metod som slumpar fram ett tal som förhåller sig mellan 0 och webbläsarens bredd minus bildens bredd
+    getLeftPosition : () => {
+        let nmbr = Math.round(Math.random() * ( window.innerWidth - 300)) + 1;
+        return nmbr;
+    },
+    // Metod som slumpar fram ett tal som förhåller sig mellan 0 och webbläsarens höjd minus bildens höjd
+    getTopPosition : () => {
+        let nmbr = Math.round(Math.random() * ( window.innerHeight - 300)) + 1;
+        return nmbr;
+    },
+    // Metod som hämtar antalet millisekunder sedan 1 januari 1970 och placerar värdet i beginning attributet
+    startTimeInMilliseconds : function() {
+        this.startTime =  Date.now();
+    },
+
+    // Metod som hämtar antalet millisekunder sedan 1 januari 1970 och placerar värdet i ending attributet
+    endTimeInMilliseconds : function() {
+        this.endTime = Date.now();
+    },
+
+    // Metod som räknar ut och returnerar antalet millisekunder det tog att fånga alla 10 pokemons
+    nmbrOfMilliseconds: () => {
+        return this.endTime - this.startTime;
+    }
+}
+
 // I denna fil skriver ni all er kod
 document.querySelector("#submitBtn").addEventListener('click', validateForm);
 document.querySelector('form').addEventListener('submit', (event) => {
@@ -44,7 +91,7 @@ function validateForm() {
                 throw new Error("Du måste vara mellan 10 och 15 år");
             } else {
                 ageInput.classList.remove('error');
-            }444444444444444444441
+            }
             for (let i = 0; i < gender.length; i++) {
                 gender[i].addEventListener('change', () => {
                   // Ta bort 'error'-klassen från alla radio-knappar
@@ -81,27 +128,87 @@ function validateForm() {
         startGame();
     }
 
-    const oGameData = {
-        pokemonNumbers: [],
-        nmbrOfCaughtPokemons: 0,
-        startTime: 0,
-        endTime: 0,
-        timerId: null,
-        trainerName: "",
-        trainerAge: 0,
-        trainerGender: "",
-    }
-
 function startGame() {
     document.querySelector('#formWrapper').classList.add('d-none');
-    document.querySelector('#highScore').classList.remove('d-none');
+    document.getElementById('gameField').classList.remove('d-none');
     oGameData.trainerName = document.querySelector('#nick').value;
     oGameData.trainerAge = document.querySelector('#age').value;
     oGameData.trainerGender = document.getElementsByName('gender');
     
     //Musiken starta när spelen starta
     const audioElement = document.getElementById('musicGame');
-    audioElement.play();
     audioElement.volume = 0.050;
+
+    audioElement.play();
+    audioElement.addEventListener('ended', () => {
+        audioElement.play();
+    });
+
+    pokemonChar();
+}
+
+function pokemonChar() {
+    const pokemonsId = Array.from({length: 151},(_,i)=>i+1);
+    oGameData.pokemonNumbers = [];
+
+    for(let i = 0; i < 5; i++) {
+        let randomPokemon = pokemonsId[Math.floor(Math.random()*pokemonsId.length)];
+        const pokemon = pokemonElement(randomPokemon);
+        oGameData.pokemonNumbers.push(randomPokemon);
+    }
+
+    setInterval(movePokemon, 3000);
+}
+
+function pokemonElement(id) {
+    const img = document.createElement('img');
+    const formattedId = String(id).padStart(3,'0');
+    img.src=`./assets/pokemons/${formattedId}.png`;
+    img.classList.add('pokemon');
+    img.dataset.id = id;
+       
+    img.onload = () => {
+        const left = oGameData.getLeftPosition(img.width);
+        const top = oGameData.getTopPosition(img.height);
+    };
+
+    document.getElementById('gameField').appendChild(img);
+    
+    
+
+    function catchPokemon() {
+
+        if (img.src = `./assets/pokemons/${formattedId}.png`) {
+            document.addEventListener("mouseover", (event) => {
+                event.target.classList.contains("pokemon");
+                event.target.src = ("./assets/ball.webp");
+                img.classList.add('ball');
+            });
+        
+        } else if (img.src = '.assets/ball.webp') {
+            document.addEventListener("mouseover", (event) => {
+                event.target.classList.contains("ball");
+                event.target.src = (`./assets/pokemons/${formattedId}.png`);
+                img.classList.add('pokemon');
+            });
+        }
+    }
+
+    catchPokemon();
+
+    return img;
+
+}
+
+function movePokemon() {
+    const pokemonArea = document.getElementById('gameField');
+    const pokemons = pokemonArea.querySelectorAll('.pokemon');
+
+    pokemons.forEach(pokemon => {
+        const randomX = Math.floor(Math.random() * (1500 - 100));
+        const randomY = Math.floor(Math.random() * (900 - 300));
+        pokemon.style.left = `${randomX}px`;
+        pokemon.style.top = `${randomY}px`;
+    });
 }
 
